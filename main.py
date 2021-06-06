9a8293b81ebfd21dd3edc9ff93b75a3864c7e95e
maila <mail.aritrasur@gmail.com>
2021-06-07 00:09:09 +0530

    Updated main.py with the creation of Spaceship class and updated its features like setting a movement speed, key pressing and drawing the health bar.
    Spaceship.png is added to the img folder.

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ clock = pygame.time.Clock()
 fps = 60
 
 screen_width = 700
-screen_height = 600
+screen_height = 635
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Space Invaders by @aritrasur47')
+
+# define colors
+red = (255, 0, 0)
+green = (0, 255, 0)
 
 # load background image
 bg = pygame.image.load('img/bg.png')
@@ -18,6 +22,42 @@ bg = pygame.image.load('img/bg.png')
 def draw_bg():
     screen.blit(bg, (0, 0))
 
+
+# Create Spaceship class
+class SpaceShip(pygame.sprite.Sprite):
+    def __init__(self, x, y, health):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/spaceship.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.health_start = health
+        self.health_remaining = health
+
+    def update(self):
+        # set movement speed
+        speed = 8
+
+        # get key press
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= speed
+        if key[pygame.K_RIGHT] and self.rect.right < screen_width:
+            self.rect.x += speed
+
+        # draw health bar
+        pygame.draw.rect(screen, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
+        if self.health_remaining > 0:
+            pygame.draw.rect(screen, green, (self.rect.x, (self.rect.bottom + 10),
+                                             int(self.rect.width * (self.health_remaining / self.health_start)),
+                                             15))
+
+
+# create sprite groups
+spaceship_group = pygame.sprite.Group()
+
+# create player
+spaceship = SpaceShip(int(screen_width / 2), screen_height - 100, 3)
+spaceship_group.add(spaceship)
 
 run = True
 while run:
@@ -30,6 +70,12 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    # update spaceship
+    spaceship.update()
+
+    # draw sprite groups
+    spaceship_group.draw(screen)
 
     pygame.display.update()
 pygame.quit()
